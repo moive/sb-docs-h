ee900d73d060a30bd306fb7f90751e65bd38d256
Moises Velasquez <moises007@gmail.com>
2026-01-09 11:47:41 -0500

    My label

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,5 +1,48 @@
 import "./MyLabel.css";
 
-export const MyLabel = () => {
-  return <span className="h1">Hello world</span>;
+interface Props {
+  /**
+   * Text to display inside the label
+   */
+  label: string;
+
+  /**
+   * Size of the label
+   */
+  size?: "normal" | "h1" | "h2" | "h3";
+
+  /**
+   * allCaps: If true, the label text will be displayed in uppercase
+   */
+  allCaps?: boolean;
+
+  /**
+   * color: Color of the label text
+   */
+  color?: "text-primary" | "text-secondary" | "text-tertiary";
+
+  /**
+   * fontColor: Custom font color for the label text
+   */
+  fontColor?: string;
+
+  /**
+   * bold: If true, the label text will be displayed in bold
+   */
+  bold?: boolean;
+}
+
+export const MyLabel = ({
+  label,
+  size = "normal",
+  allCaps = false,
+  color,
+  fontColor,
+  bold = false,
+}: Props) => {
+  return (
+    <span className={`label ${size} ${color} ${bold ? "bold" : ""}`} style={{ color: fontColor }}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
 };
--- src/stories/MyLabel.stories.ts
+++ src/stories/MyLabel.stories.ts
@@ -4,8 +4,13 @@ import { MyLabel } from "../components/MyLabel";
 const meta = {
   title: "UI/labels/MyLabel",
   component: MyLabel,
+  tags: ["autodocs"],
   parameters: {
     layout: "centered",
+  },
+  argTypes: {
+    size: { control: "inline-radio" },
+    fontColor: { control: "color" },
   },
 } satisfies Meta<typeof MyLabel>;
 
@@ -13,4 +18,33 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {};
+export const Basic: Story = {
+  args: {
+    label: "Basic Label",
+  },
+};
+export const AllCaps: Story = {
+  args: {
+    label: "All Caps Label",
+    allCaps: true,
+  },
+};
+export const Secondary: Story = {
+  args: {
+    label: "Secondary Label",
+    color: "text-secondary",
+  },
+};
+export const CustomColor: Story = {
+  args: {
+    label: "Custom Color Label",
+    fontColor: "#5517ac",
+  },
+};
+
+export const Bold: Story = {
+  args: {
+    label: "Bold Label",
+    bold: true,
+  },
+};
